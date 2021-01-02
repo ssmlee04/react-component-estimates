ab79aa5cb50b23476650d9e9353aef41a80c10b2
Shih-Min Lee <ssmlee04@gmail.com>
2021-01-02 15:21:46 -0500

    only plot eps and estimates now

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = exports.Analyst = void 0;
+exports["default"] = exports.Estimates = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -79,112 +79,22 @@ var formatNumber2 = function formatNumber2(number) {
   return number;
 };
 
-var calculateMargins = function calculateMargins(data) {
-  var divider = 1000;
-  var unit = 'thousands';
-  var u = 'k';
-  if (!data || !data.length) return data;
-
-  if (data[0].rev > 10000000) {
-    divider = 1000000;
-    unit = 'milllion';
-    u = 'm';
-  }
-
-  if (data[0].rev > 10000000000) {
-    divider = 1000000000;
-    unit = 'billion';
-    u = 'b';
-  }
-
-  data = _lodash["default"].sortBy(data.filter(function (d) {
-    return d.reportDate;
-  }), function (d) {
-    return -d.reportDate;
-  }).reverse();
-  return data.map(function (d, i) {
-    var qq = ~~d.reportDate.slice(5, 7);
-    var yy = d.reportDate.slice(0, 4);
-    var qtr;
-
-    if (qq <= 3) {
-      qtr = 'Q1';
-    } else if (qq <= 6) {
-      qtr = 'Q2';
-    } else if (qq <= 9) {
-      qtr = 'Q3';
-    } else if (qq <= 12) {
-      qtr = 'Q4';
-    }
-
-    d.unit = unit;
-    d.u = u;
-    d.cogsSmall = d.cogs / divider;
-    d.ebitSmall = d.ebit / divider;
-    d.gpSmall = d.gp / divider;
-    d.incomeTaxSmall = d.incomeTax / divider;
-    d.niSmall = d.ni / divider;
-    d.oiSmall = d.oi / divider;
-    d.operatingExpenseSmall = d.operatingExpense / divider;
-    d.otherIncomeExpenseSmall = d.otherIncomeExpense / divider;
-    d.rndSmall = d.rnd / divider;
-    d.sgnaSmall = d.sgna / divider;
-    d.revSmall = d.rev / divider;
-    d.revenueGrowthYoy = data[i - 4] ? ((d.rev / data[i - 4].rev - 1) * 100).toFixed(2) : '';
-    d.quarterStr = yy + qtr;
-    d.gpMargin = parseFloat((d.gp / d.rev * 100).toFixed(2));
-    d.oiMargin = parseFloat((d.oi / d.rev * 100).toFixed(2));
-    d.ebitMargin = parseFloat((d.ebit / d.rev * 100).toFixed(2));
-    d.niMargin = parseFloat((d.ni / d.rev * 100).toFixed(2));
-    return d;
-  });
-};
-
-var calculateYearly = function calculateYearly(data) {
-  data = _lodash["default"].sortBy(data.filter(function (d) {
-    return d.reportDate;
-  }), function (d) {
-    return -d.reportDate;
-  }).reverse();
-  var years = {};
-  var arr = [];
-  data.forEach(function (d, i) {
-    var yy = d.reportDate.slice(0, 4);
-
-    if (years[yy] === undefined) {
-      years[yy] = arr.length;
-    }
-
-    arr[years[yy]] = arr[years[yy]] || {};
-    arr[years[yy]].c = arr[years[yy]].c || 0;
-    arr[years[yy]].year = arr[years[yy]].year || yy;
-    arr[years[yy]].ni = arr[years[yy]].ni || 0;
-    arr[years[yy]].ni += d.ni;
-    arr[years[yy]].rev = arr[years[yy]].rev || 0;
-    arr[years[yy]].rev += d.rev;
-    arr[years[yy]].c++;
-  });
-  return arr.filter(function (d) {
-    return d.c === 4;
-  });
-};
-
-var Analyst =
+var Estimates =
 /*#__PURE__*/
 function (_React$Component) {
-  _inherits(Analyst, _React$Component);
+  _inherits(Estimates, _React$Component);
 
-  function Analyst(props) {
+  function Estimates(props) {
     var _this;
 
-    _classCallCheck(this, Analyst);
+    _classCallCheck(this, Estimates);
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Analyst).call(this, props));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Estimates).call(this, props));
     _this.state = {};
     return _this;
   }
 
-  _createClass(Analyst, [{
+  _createClass(Estimates, [{
     key: "shouldComponentUpdate",
     value: function shouldComponentUpdate(nextProps, nextState) {
       var profile = this.props.profile;
@@ -198,30 +108,35 @@ function (_React$Component) {
     value: function render() {
       var _this2 = this;
 
-      var profile = this.props.profile;
+      var _this$props = this.props,
+          profile = _this$props.profile,
+          _this$props$imgProp = _this$props.imgProp,
+          imgProp = _this$props$imgProp === void 0 ? 'estimates_av_img' : _this$props$imgProp,
+          _this$props$prop = _this$props.prop,
+          prop = _this$props$prop === void 0 ? 'earnings_av.data' : _this$props$prop;
       var copied = this.state.copied;
 
       if (!profile) {
         return _react["default"].createElement("div", {
           style: {
-            fontSize: 14
+            fontSize: 8
           }
         }, "Not available at this time... ");
       }
 
-      if (profile.estimate_img && profile.estimate_img.url) {
+      if (profile[imgProp] && profile[imgProp].url) {
         var btnClass = copied ? 'react-components-show-url btn btn-sm btn-danger disabled font-10' : 'react-components-show-url btn btn-sm btn-warning font-10';
         var btnText = copied ? 'Copied' : 'Copy Img';
         return _react["default"].createElement("div", {
           className: "react-components-show-button"
         }, _react["default"].createElement("img", {
           alt: "".concat(profile.ticker, " - ").concat(profile.name, " earnings estimates"),
-          src: profile.estimate_img.url,
+          src: profile[imgProp].url,
           style: {
             width: '100%'
           }
         }), _react["default"].createElement(_reactCopyToClipboard.CopyToClipboard, {
-          text: profile.estimate_img.url || '',
+          text: profile[imgProp].url || '',
           onCopy: function onCopy() {
             return _this2.setState({
               copied: true
@@ -233,51 +148,24 @@ function (_React$Component) {
         }, btnText)));
       }
 
-      if (!profile) {
-        return _react["default"].createElement("div", {
-          className: "font-12"
-        }, "Not available at this time... ");
-      }
-
-      var data = _lodash["default"].get(profile, 'income_and_revenue.data', []);
+      var data = _lodash["default"].get(profile, prop, []);
 
       if (!data || !data.length) {
         return _react["default"].createElement("div", {
-          className: "font-12"
+          className: "font-8"
         }, "Not available at this time... ");
       }
 
       data = data.map(function (d) {
-        d.reportDate = d.reportDate.replace(/-/g, '').slice(0, 6);
+        d.fiscalDate = d.fiscalDate.replace(/-/g, '').slice(0, 6);
         return d;
-      });
-      var yearly = calculateYearly(data);
-      var quarterly = calculateMargins(data.slice(0, 12));
-      var minQuarterly = quarterly.reduce(function (t, d) {
-        return Math.min(t, d.rev);
-      }, 9999999999999999) / 2;
-      var minYearly = yearly.reduce(function (t, d) {
-        return Math.min(t, d.rev);
-      }, 9999999999999999) / 2;
-      var unit = data && data[0].unit;
-      var optionsQuarterly = {
-        tooltips: {
-          callbacks: {
-            label: function label(tooltipItem, data) {
-              var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-              if (label) {
-                label += ': ';
-              }
-
-              label += formatNumber(tooltipItem.yLabel);
-              return label;
-            }
-          }
-        },
+      }).sort(function (a, b) {
+        return a.fiscalDate > b.fiscalDate;
+      }).slice(-12);
+      var options = {
         legend: {
           labels: {
-            fontSize: 12,
+            fontSize: 8,
             boxWidth: 12
           }
         },
@@ -292,234 +180,78 @@ function (_React$Component) {
           yAxes: [{
             type: 'linear',
             display: true,
-            position: 'right',
-            id: 'earnings',
-            gridLines: {
-              display: false
-            },
             labels: {
               show: true
             },
             ticks: {
               fontSize: 10,
-              fontColor: 'orange',
               callback: function callback(label, index, labels) {
                 return formatNumber2(label, 0);
-              }
-            }
-          }, {
-            type: 'linear',
-            display: true,
-            position: 'left',
-            id: 'revenue',
-            labels: {
-              show: true
-            },
-            ticks: {
-              fontSize: 10,
-              min: minQuarterly,
-              callback: function callback(label, index, labels) {
-                return formatNumber(label, 0);
               }
             }
           }]
         }
       };
-      var optionsYearly = {
-        tooltips: {
-          callbacks: {
-            label: function label(tooltipItem, data) {
-              var label = data.datasets[tooltipItem.datasetIndex].label || '';
 
-              if (label) {
-                label += ': ';
-              }
+      var estimatesLastFiscalDate = _lodash["default"].last(data).fiscalDate;
 
-              label += formatNumber(tooltipItem.yLabel);
-              return label;
-            }
-          }
-        },
-        legend: {
-          labels: {
-            fontSize: 12,
-            boxWidth: 12
-          }
-        },
-        scales: {
-          xAxes: [{
-            ticks: {
-              autoSkip: false,
-              fontSize: 10
-            },
-            barPercentage: 0.2
-          }],
-          yAxes: [{
-            type: 'linear',
-            display: true,
-            position: 'right',
-            id: 'earnings',
-            gridLines: {
-              display: false
-            },
-            labels: {
-              show: true
-            },
-            ticks: {
-              fontSize: 10,
-              fontColor: 'orange',
-              callback: function callback(label, index, labels) {
-                return formatNumber2(label, 0);
-              }
-            }
-          }, {
-            type: 'linear',
-            display: true,
-            position: 'left',
-            id: 'revenue',
-            labels: {
-              show: true
-            },
-            ticks: {
-              fontSize: 10,
-              min: minYearly,
-              callback: function callback(label, index, labels) {
-                return formatNumber(label, 0);
-              }
-            }
-          }]
-        }
-      };
-      var yearlyData = {
-        labels: yearly.map(function (d) {
-          return d.year;
-        }),
-        datasets: [{
-          label: "Yearly Earnings (".concat(unit, ")"),
-          type: 'line',
-          fill: false,
-          lineTension: 0,
-          borderWidth: 0,
-          backgroundColor: 'orange',
-          borderColor: 'orange',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'gray',
-          pointBackgroundColor: 'white',
-          pointBorderWidth: 2,
-          pointHoverRadius: 5,
-          pointHoverBorderWidth: 2,
-          pointRadius: 2,
-          pointHitRadius: 6,
-          yAxisID: 'earnings',
-          data: yearly.map(function (d) {
-            return d.ni;
-          })
-        }, {
-          label: "Annual Revenue (".concat(unit, ")"),
-          type: 'bar',
-          lineTension: 0,
-          backgroundColor: '#368BC1',
-          borderColor: '#368BC1',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'gray',
-          pointBackgroundColor: 'white',
-          pointBorderWidth: 2,
-          pointHoverRadius: 5,
-          pointHoverBorderWidth: 2,
-          pointRadius: 2,
-          pointHitRadius: 6,
-          yAxisID: 'revenue',
-          data: yearly.map(function (d) {
-            return d.rev;
-          })
-        }]
-      };
+      var CurrentEstimate = _lodash["default"].get(profile, 'estimates_yh.earnings_0q', {});
 
-      var estimate = _lodash["default"].get(profile, 'estimate.data.consensusEPS', null);
+      var CurrentEstimateFiscalDate = CurrentEstimate.endDate && CurrentEstimate.endDate.replace(/-/g, '').slice(0, 6);
+      var CurrentEstimateEps;
 
-      var earningsData = JSON.parse(JSON.stringify(_lodash["default"].get(profile, 'earnings.data', []))).reverse();
-      var earningsDataAndEstimate = earningsData.concat([{
-        fiscalPeriod: 'est',
-        consensusEPS: estimate
-      }]);
+      if (CurrentEstimateFiscalDate > estimatesLastFiscalDate) {
+        CurrentEstimateEps = CurrentEstimate.avg;
+        data = data.concat([{
+          estmiate: CurrentEstimateEps,
+          // estmiate: -0.04,
+          fiscalDate: CurrentEstimateFiscalDate
+        }]);
+      }
+
       var quarterlyData = {
         // labels: quarterly.map(d => d.reportDate),
-        labels: earningsDataAndEstimate.map(function (d) {
-          return d.fiscalPeriod;
+        labels: data.map(function (d) {
+          return d.fiscalDate;
         }),
         datasets: [{
           label: 'Actual EPS',
           fill: false,
           type: 'line',
-          lineTension: 0,
+          lineTension: 0.3,
           borderWidth: 1,
-          backgroundColor: 'orange',
-          borderColor: 'orange',
-          borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'red',
-          pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointStyle: 'rectRot',
-          pointRadius: 2,
-          pointHitRadius: 20,
-          data: earningsData.map(function (d) {
-            return d.actualEPS;
+          // pointStyle: 'rectRot',
+          pointRadius: 3,
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgba(255, 99, 132)',
+          // pointHitRadius: 20,
+          data: data.map(function (d) {
+            return d.eps;
           })
         }, {
           label: 'Estimate EPS',
           fill: false,
           type: 'line',
-          lineTension: 0,
+          lineTension: 0.3,
           borderWidth: 1,
-          backgroundColor: 'rgba(175,14,14,1)',
-          borderColor: 'rgba(175,14,14,1)',
-          borderCapStyle: 'butt',
+          // borderCapStyle: 'butt',
           borderDash: [10, 5],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'red',
-          pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointStyle: 'circle',
-          pointRadius: 2,
-          pointHitRadius: 20,
-          data: earningsDataAndEstimate.map(function (d) {
-            return d.consensusEPS;
-          })
-        }, {
-          label: "Revenue (".concat(unit, ")"),
-          type: 'bar',
-          lineTension: 0,
-          backgroundColor: '#368BC1',
-          borderColor: '#368BC1',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'gray',
-          pointBackgroundColor: 'white',
-          pointBorderWidth: 2,
-          pointHoverRadius: 5,
-          pointHoverBorderWidth: 2,
-          pointRadius: 2,
-          pointHitRadius: 6,
-          yAxisID: 'revenue',
-          data: quarterly.slice(0, earningsDataAndEstimate.length - 1).map(function (d) {
-            return d.rev;
+          // pointStyle: 'circle',
+          pointRadius: 3,
+          pointBackgroundColor: 'rgb(66, 33, 199)',
+          backgroundColor: 'rgb(66, 33, 199)',
+          borderColor: 'rgba(66, 33, 199)',
+          // pointHitRadius: 20,
+          data: data.map(function (d) {
+            return d.estmiate;
           })
         }]
       };
@@ -527,40 +259,51 @@ function (_React$Component) {
         style: {
           width: '100%',
           padding: 5,
-          fontSize: 14
+          fontSize: 8
         }
       }, _react["default"].createElement("div", {
         style: {
           color: 'darkred',
           fontWeight: 'bold'
         }
-      }, profile.ticker, " - ", profile.name), estimate ? _react["default"].createElement("span", {
+      }, profile.ticker, " - ", profile.name, _react["default"].createElement("span", {
         style: {
-          fontSize: 10
+          marginLeft: 5,
+          color: 'green'
+        }
+      }, "EPS Estimates")), CurrentEstimateEps ? _react["default"].createElement("span", {
+        style: {
+          fontSize: 8
         }
       }, "Current Qtr Estimate (dashed line): ", _react["default"].createElement("b", {
         style: {
           color: 'crimson'
         }
-      }, estimate)) : null, quarterlyData ? _react["default"].createElement(_reactChartjs.Bar, {
-        options: optionsQuarterly,
+      }, CurrentEstimateEps)) : null, quarterlyData ? _react["default"].createElement(_reactChartjs.Bar, {
+        options: options,
         data: quarterlyData,
-        height: 150
-      }) : null, _react["default"].createElement("hr", {
+        height: 180
+      }) : null, _react["default"].createElement("div", {
         style: {
-          margin: 1
+          fontSize: 8,
+          color: 'gray'
         }
-      }), yearlyData ? _react["default"].createElement(_reactChartjs.Bar, {
-        options: optionsYearly,
-        data: yearlyData,
-        height: 150
-      }) : null);
+      }, "Generated by ", _react["default"].createElement("span", {
+        style: {
+          color: 'darkred'
+        }
+      }, "@earningsfly"), " with ", _react["default"].createElement("span", {
+        style: {
+          fontSize: 16,
+          color: 'red'
+        }
+      }, "\uD83D\uDE80")));
     }
   }]);
 
-  return Analyst;
+  return Estimates;
 }(_react["default"].Component);
 
-exports.Analyst = Analyst;
-var _default = Analyst;
+exports.Estimates = Estimates;
+var _default = Estimates;
 exports["default"] = _default;
--- src/index.js
+++ src/index.js
@@ -36,64 +36,6 @@ const formatNumber2 = (number, toFix = 2) => {
   return number;
 };
 
-const calculateMargins = (data) => {
-  let divider = 1000;
-  let unit = 'thousands';
-  let u = 'k';
-  if (!data || !data.length) return data;
-  if (data[0].rev > 10000000) {
-    divider = 1000000;
-    unit = 'milllion';
-    u = 'm';
-  }
-  if (data[0].rev > 10000000000) {
-    divider = 1000000000;
-    unit = 'billion';
-    u = 'b';
-  }
-  data = _.sortBy(data.filter(d => d.reportDate), (d) => {
-    return -d.reportDate;
-  }).reverse();
-
-  return data.map((d, i) => {
-    const qq = ~~d.reportDate.slice(5, 7);
-    let yy =d.reportDate.slice(0, 4);
-    let qtr;
-    if (qq <= 3) {
-      qtr = 'Q1';
-    }
-    else if (qq <= 6) {
-      qtr = 'Q2';
-    }
-    else if (qq <= 9) {
-      qtr = 'Q3';
-    }
-    else if (qq <= 12) {
-      qtr = 'Q4';
-    }
-    d.unit = unit;
-    d.u = u;
-    d.cogsSmall = d.cogs / divider;
-    d.ebitSmall = d.ebit / divider;
-    d.gpSmall = d.gp / divider;
-    d.incomeTaxSmall = d.incomeTax / divider;
-    d.niSmall = d.ni / divider;
-    d.oiSmall = d.oi / divider;
-    d.operatingExpenseSmall = d.operatingExpense / divider;
-    d.otherIncomeExpenseSmall = d.otherIncomeExpense / divider;
-    d.rndSmall = d.rnd / divider;
-    d.sgnaSmall = d.sgna / divider;
-    d.revSmall = d.rev / divider;
-    d.revenueGrowthYoy = data[i - 4] ? ((d.rev / data[i - 4].rev - 1) * 100).toFixed(2) : '';
-    d.quarterStr = yy + qtr;
-    d.gpMargin = parseFloat((d.gp / d.rev * 100).toFixed(2));
-    d.oiMargin = parseFloat((d.oi / d.rev * 100).toFixed(2));
-    d.ebitMargin = parseFloat((d.ebit / d.rev * 100).toFixed(2));
-    d.niMargin = parseFloat((d.ni / d.rev * 100).toFixed(2));
-    return d;
-  });
-};
-
 export class Estimates extends React.Component {
   constructor(props) {
     super(props);
@@ -111,11 +53,11 @@ export class Estimates extends React.Component {
   }
 
   render() {
-    const { profile, imgProp = 'estimates_img', prop = 'earnings_av.data' } = this.props;
+    const { profile, imgProp = 'estimates_av_img', prop = 'earnings_av.data' } = this.props;
     const { copied } = this.state;
     if (!profile) {
       return (
-        <div style={{ fontSize: 14 }}>Not available at this time... </div>
+        <div style={{ fontSize: 8 }}>Not available at this time... </div>
       );
     }
     if (profile[imgProp] && profile[imgProp].url) {
@@ -141,7 +83,7 @@ export class Estimates extends React.Component {
     data = data.map(d => {
       d.fiscalDate = d.fiscalDate.replace(/-/g, '').slice(0, 6);
       return d;
-    }).slice(0, 12);
+    }).sort((a, b) => a.fiscalDate > b.fiscalDate).slice(-12);
 
     var options = {
       legend: {
@@ -164,7 +106,6 @@ export class Estimates extends React.Component {
             },
             ticks: {
               fontSize: 10,
-                fontColor: 'orange',
                 callback: function(label, index, labels) {
                   return formatNumber2(label, 0);
                 }
@@ -181,7 +122,7 @@ export class Estimates extends React.Component {
     if (CurrentEstimateFiscalDate > estimatesLastFiscalDate) {
       CurrentEstimateEps = CurrentEstimate.avg
       data = data.concat([{
-        eps: -0.05,
+        estmiate: CurrentEstimateEps,
         // estmiate: -0.04,
         fiscalDate: CurrentEstimateFiscalDate,
       }])
@@ -197,22 +138,16 @@ export class Estimates extends React.Component {
           type:'line',
           lineTension: 0.3,
           borderWidth: 1,
-          backgroundColor: 'orange',
-          borderColor: 'orange',
-          borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'red',
-          pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointStyle: 'rectRot',
-          pointRadius: 2,
-          pointHitRadius: 20,
+          // pointStyle: 'rectRot',
+          pointRadius: 3,
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgba(255, 99, 132)',
+          // pointHitRadius: 20,
           data: data.map(d => d.eps)
         },
         {
@@ -221,22 +156,17 @@ export class Estimates extends React.Component {
           type:'line',
           lineTension: 0.3,
           borderWidth: 1,
-          backgroundColor: 'rgba(175,14,14,1)',
-          borderColor: 'rgba(175,14,14,1)',
-          borderCapStyle: 'butt',
+          // borderCapStyle: 'butt',
           borderDash: [10,5],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'red',
-          pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(0,100,0,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointStyle: 'circle',
-          pointRadius: 2,
-          pointHitRadius: 20,
+          // pointStyle: 'circle',
+          pointRadius: 3,
+          pointBackgroundColor: 'rgb(66, 33, 199)',
+          backgroundColor: 'rgb(66, 33, 199)',
+          borderColor: 'rgba(66, 33, 199)',
+          // pointHitRadius: 20,
           data: data.map(d => d.estmiate)
         },
       ]
@@ -244,7 +174,7 @@ export class Estimates extends React.Component {
 
     return (
       <div style={{ width: '100%', padding: 5, fontSize: 8 }}>
-        <div style={{ color: 'darkred', fontWeight: 'bold' }}>{profile.ticker} - {profile.name}</div>
+        <div style={{ color: 'darkred', fontWeight: 'bold' }}>{profile.ticker} - {profile.name}<span style={{ marginLeft: 5, color: 'green' }}>EPS Estimates</span></div>
         {CurrentEstimateEps ? <span style={{ fontSize: 8 }}>Current Qtr Estimate (dashed line): <b style={{ color: 'crimson' }}>{CurrentEstimateEps}</b></span> : null}
         {quarterlyData ? <Bar options={options} data={quarterlyData} height={180} /> : null}
         <div style={{ fontSize: 8, color: 'gray' }}>Generated by <span style={{ color: 'darkred' }}>@earningsfly</span> with <span style={{ fontSize: 16, color: 'red' }}>🚀</span></div>
